f4f2c02240d54e5dc1906a15483ada30f1637406
Adam C. Smith <acsmith843@gmail.com>
2013-11-13 10:10:44 -0500

    adding search cell to project

---
+++ SmartTravel/SearchCell.h
@@ -0,0 +1,13 @@
+//
+//  SearchCell.h
+//  SmartTravel
+//
+//  Created by Adam C. Smith on 11/13/13.
+//  Copyright (c) 2013 SPARC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchCell : UITableViewCell
+
+@end
