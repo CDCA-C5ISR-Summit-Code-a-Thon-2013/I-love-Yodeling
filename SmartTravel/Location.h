e068796a8bca3df102dc7cea72d10029cab70d5b
Adam C. Smith <acsmith843@gmail.com>
2013-11-13 15:42:40 -0500

    adding logo to location object

--- SmartTravel/Location.h
+++ SmartTravel/Location.h
@@ -14,9 +14,10 @@
 @property (nonatomic, readonly) NSString *name;
 @property (nonatomic, readonly) NSString *address;
 @property (nonatomic, readonly) NSString *image;
+@property (nonatomic, readonly) NSString *logo;
 @property (nonatomic, readonly) NSString *dealText;
 @property (readonly) CLLocationCoordinate2D coordinate;
 
-- (id)initWithName:(NSString*)name andAddress:(NSString*)address andImage:(NSString*)image andDealText:(NSString*)dealText andCoordinate:(CLLocationCoordinate2D)coordinate;
+- (id)initWithName:(NSString*)name andAddress:(NSString*)address andImage:(NSString*)image andLogo:(NSString *)logo andDealText:(NSString*)dealText andCoordinate:(CLLocationCoordinate2D)coordinate;
 
 @end
--- SmartTravel/Location.m
+++ SmartTravel/Location.m
@@ -10,13 +10,14 @@
 
 @implementation Location
 
-- (id)initWithName:(NSString*)name andAddress:(NSString*)address andImage:(NSString*)image andDealText:(NSString*)dealText andCoordinate:(CLLocationCoordinate2D)coordinate
+- (id)initWithName:(NSString*)name andAddress:(NSString*)address andImage:(NSString*)image andLogo:(NSString *)logo andDealText:(NSString*)dealText andCoordinate:(CLLocationCoordinate2D)coordinate
 {
     self = [super init];
     _name = name;
     _address = address;
     _coordinate = coordinate;
     _image = image;
+    _logo = logo;
     _dealText = dealText;
     return self;
 }
