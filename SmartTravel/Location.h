629419aa8d87739f716cba2e90ca0dd6effb1eb3
Will Goss <will.goss@sparcedge.com>
2013-11-13 11:40:32 -0500

    Added Location class

---
+++ SmartTravel/Location.h
@@ -0,0 +1,13 @@
+//
+//  Location.h
+//  SmartTravel
+//
+//  Created by Will Goss on 11/13/13.
+//  Copyright (c) 2013 SPARC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Location : NSObject
+
+@end
