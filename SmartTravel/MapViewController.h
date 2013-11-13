4f2fad181126d35d0d9ce5081c93579117e76f9a
Adam C. Smith <acsmith843@gmail.com>
2013-11-13 09:41:14 -0500

    adding initial view controllers

---
+++ SmartTravel/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  SmartTravel
+//
+//  Created by Adam C. Smith on 11/13/13.
+//  Copyright (c) 2013 SPARC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
---
+++ SmartTravel/SearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.h
+//  SmartTravel
+//
+//  Created by Adam C. Smith on 11/13/13.
+//  Copyright (c) 2013 SPARC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchViewController : UIViewController
+
+@end
