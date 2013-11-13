0836eef90ec4be7151d5efbd7e07a600b3d85957
bobby.strickland <bcstrickland@gmail.com>
2013-11-13 10:41:51 -0500

    Bookmark object and more bookmark table imp

---
+++ SmartTravel/Bookmark.m
@@ -0,0 +1,13 @@
+//
+//  Bookmark.m
+//  SmartTravel
+//
+//  Created by Bobby Strickland on 11/13/13.
+//  Copyright (c) 2013 SPARC. All rights reserved.
+//
+
+#import "Bookmark.h"
+
+@implementation Bookmark
+
+@end
