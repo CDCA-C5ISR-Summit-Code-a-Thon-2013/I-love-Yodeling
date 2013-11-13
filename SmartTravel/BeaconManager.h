811d501859f56b6346e64ef1dce6f93b3de5df13
bobby.strickland <bcstrickland@gmail.com>
2013-11-13 09:56:19 -0500

    Init beacon manager

---
+++ SmartTravel/BeaconManager.h
@@ -0,0 +1,13 @@
+//
+//  BeaconManager.h
+//  SmartTravel
+//
+//  Created by Bobby Strickland on 11/13/13.
+//  Copyright (c) 2013 SPARC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BeaconManager : NSObject
+
+@end
