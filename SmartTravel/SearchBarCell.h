70c10347d25eb7ded7f45083669ac260b40266a1
Adam C. Smith <acsmith843@gmail.com>
2013-11-13 10:52:40 -0500

    adding search bar cell to project

---
+++ SmartTravel/SearchBarCell.h
@@ -0,0 +1,15 @@
+//
+//  SearchBarCell.h
+//  SmartTravel
+//
+//  Created by Adam C. Smith on 11/13/13.
+//  Copyright (c) 2013 SPARC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchBarCell : UITableViewCell
+
+@property (strong, nonatomic) IBOutlet UISearchBar *searchBar;
+
+@end
